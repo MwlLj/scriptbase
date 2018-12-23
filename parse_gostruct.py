159926e733e355b9a556a88e1f65f871a52dede6
MwlLj <731025894@qq.com>
2018-12-23 10:27:33 +0800

    适配list 和 map 类型

--- parse_gostruct.py
+++ parse_gostruct.py
@@ -10,6 +10,10 @@ class CGoStructParse(CFileReader):
 	PARAM_LIST = "param_list"
 	PARAM_TYPE = "param_type"
 	PARAM_NAME = "param_name"
+	PARAM_TYPE_MAP_KEY = "param_type_map_key"
+	PARAM_TYPE_MAP_VALUE = "param_type_map_value"
+	PARAM_IS_LIST = "param_is_list"
+	PARAM_IS_MAP = "param_is_map"
 	REFLEX_CONTENT = "reflex_content"
 
 	def __init__(self, file_path):
@@ -47,11 +51,33 @@ class CGoStructParse(CFileReader):
 					raise SystemExit("[ERROR] sturct {0}: field error".format(struct_name))
 				param_name = None
 				param_type = None
+				param_type_map_key = None
+				param_type_map_value = None
 				reflex_content = None
+				param_is_list = False
+				param_is_map = False
 				if values_len == 2:
 					param_name, param_type = result
 				else:
 					param_name, param_type, reflex_content = result
+				search_result = re.search(r"\[\](.*)?", param_type)
+				if search_result is not None:
+					search_groups = search_result.groups()
+					if len(search_groups) == 1:
+						param_type = search_groups[0]
+						param_is_list = True
+				search_result = re.search(r"map\[(.*?)\](.*)?", param_type)
+				if search_result is not None:
+					search_groups = search_result.groups()
+					if len(search_groups) == 2:
+						param_type = None
+						param_type_map_key = search_groups[0]
+						param_type_map_value = search_groups[1]
+						param_is_map = True
+				param[CGoStructParse.PARAM_IS_LIST] = param_is_list
+				param[CGoStructParse.PARAM_IS_MAP] = param_is_map
+				param[CGoStructParse.PARAM_TYPE_MAP_KEY] = param_type_map_key
+				param[CGoStructParse.PARAM_TYPE_MAP_VALUE] = param_type_map_value
 				param[CGoStructParse.PARAM_NAME] = param_name
 				param[CGoStructParse.PARAM_TYPE] = param_type
 				param[CGoStructParse.REFLEX_CONTENT] = reflex_content
